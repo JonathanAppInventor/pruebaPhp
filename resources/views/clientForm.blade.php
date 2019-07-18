10d409a6633da0d879c734caea742b02d2a6fbd6
JonathanAppInventor <52981652+JonathanAppInventor@users.noreply.github.com>
2019-07-17 20:55:57 -0500

    Update clientForm.blade.php

--- resources/views/clientForm.blade.php
+++ resources/views/clientForm.blade.php
@@ -1,5 +1,20 @@
 @extends ('template')
 
 @section ('section')
-<h1>This an inventory add page</h1>
+<h1>Purchases sheet!</h1>
+<div class="container my-4">
+<h1 class="display-4">Products</h1>
+<table class="table">
+<thead>
+<tr>
+  <th scope="col">#id</th>
+  <th scope="col">Product</th>
+  <th scope="col">Quantity</th>
+</tr>
+</thead>
+<tbody>
+
+</tbody>
+</table>
+</div>
 @endsection
